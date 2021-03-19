936ed68407d2ec9905ba40aa9db8ef5a812bbf72
WailAbou <abou.w@hotmail.com>
2021-03-19 18:16:04 +0100

    - Improved the tests, added analysis, added design

--- summatieve_opdrachten/P3/threaded_merge_sort_test.py
+++ summatieve_opdrachten/P3/threaded_merge_sort_test.py
@@ -1,13 +1,28 @@
 import threaded_merge_sort as tms 
 import numpy as np
+import time
 
 
-def check_sort(size, threads=8, min_num=-100.0, max_num=100.0):
+def time_merge_sort(size, threads=8, min_num=-100.0, max_num=100.0):
     '''Creates an array of unsorted data, sort it and checks if it sorted correctly.'''
     unsorted_data = np.random.uniform(min_num, max_num, size)
-    sorted_data = tms.merge_sort_threaded(unsorted_data, threads)
-    succes = sorted(unsorted_data) == sorted_data
-    print(f'The data with size {size} using {threads} threads has been succesfully sorted = {succes}')
+
+    start_single = time.time()
+    sorted_data_single = tms.merge_sort(unsorted_data)
+    end_single = time.time()
+    succes = sorted(unsorted_data) == sorted_data_single
+    
+    print(f'Time taken for single threaded: {end_single - start_single}')
+    print(f'The data with size {size} using single thread has been succesfully sorted = {succes}\n')
+
+    start_multi = time.time()
+    sorted_data_multi = tms.merge_sort_threaded(unsorted_data, threads)
+    end_multi = time.time()
+    succes = sorted(unsorted_data) == sorted_data_multi
+
+    print(f'Time taken for multi threaded: {end_multi - start_multi}')
+    print(f'The data with size {size} using {threads} threads has been succesfully sorted = {succes}\n')
+   
 
 if __name__ == "__main__":
     '''Tests 4 different situations
@@ -15,7 +30,7 @@ if __name__ == "__main__":
     2. even number data points and odd number threads
     3. odd number data points and even number threads
     4. odd number data points and odd number threads'''
-    check_sort(80, 8)
-    check_sort(80, 7)
-    check_sort(81, 8)
-    check_sort(81, 7)
+    time_merge_sort(80, 8)
+    time_merge_sort(80, 7)
+    time_merge_sort(81, 8)
+    time_merge_sort(81, 7)
